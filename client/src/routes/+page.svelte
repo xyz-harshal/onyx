d8edf11b2c1e0455fea21188f4ef9eee8f1a138f
harshal-ishere <harshal011kamble@gmail.com>
2024-08-31 14:34:17 +0530

    updated the frontend

--- client/src/routes/+page.svelte
+++ client/src/routes/+page.svelte
@@ -1,8 +1,8 @@
 <script>
-  import '../app.css'
+    import "../app.css";
 </script>
 
-<div class="hero bg-base-200 min-h-screen">
+<!-- <div class="hero bg-base-200 min-h-screen">
   <div class="hero-content text-center">
     <div class="max-w-md">
       <h1 class="text-5xl font-bold">Hello there</h1>
@@ -13,4 +13,19 @@
       <button class="btn btn-primary">Get Started</button>
     </div>
   </div>
+</div> -->
+<div class="card bg-base-100 w-96 shadow-xl">
+    <figure>
+        <img
+            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+            alt="Shoes"
+        />
+    </figure>
+    <div class="card-body">
+        <h2 class="card-title">Shoes!</h2>
+        <p>If a dog chews shoes whose shoes does he choose?</p>
+        <div class="card-actions justify-end">
+            <button class="btn btn-primary">Buy Now</button>
+        </div>
+    </div>
 </div>
---
+++ client/src/routes/main/+page.svelte
@@ -0,0 +1,104 @@
+<script>
+    import "../../app.css";
+</script>
+
+<div class="flex w-full">
+    <div class="card grid h-screen flex-grow justify-center pb-6">
+        <div class="hero">
+            <div class="hero-content text-center">
+                <div class="max-w-lg">
+                    <h1 class="text-5xl font-bold">
+                        Hyperspectral Anomaly Detection: A Powerful Tool for
+                        Data Analysis
+                    </h1>
+                </div>
+            </div>
+        </div>
+        <div class="flex fex-row gap-4 place-items-end justify-center">
+            <input
+                type="file"
+                class="file-input file-input-primary file-input-lg w-full max-w-xs"
+            />
+            <button
+                class="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg btn-outline btn-primary"
+                >Submit</button
+            >
+        </div>
+    </div>
+
+    <div class="divider divider-horizontal"></div>
+    <div class="card h-screen w-6/12 flex flex-col">
+        <!-- <kbd class="kbd kbd-lg mb-6 mt-6 self-center">Characteristics</kbd> -->
+        <h1 class="text-5xl font-bold flex flex-row justify-center mb-6 mt-6">
+            Characteristics
+        </h1>
+        <div class="flex-grow overflow-y-auto px-4">
+            <div
+                class="flex flex-row flex-wrap items-start justify-center gap-10 pb-6"
+            >
+                <div class="card bg-base-100 w-96 shadow-xl">
+                    <figure>
+                        <img
+                            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                            alt="Shoes"
+                        />
+                    </figure>
+                    <div class="card-body">
+                        <h2 class="card-title">Shoes!</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div class="card-actions justify-center">
+                            <button class="btn btn-primary btn-block"
+                                >Upload</button
+                            >
+                        </div>
+                    </div>
+                </div>
+                <div class="card bg-base-100 w-96 shadow-xl">
+                    <figure>
+                        <img
+                            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                            alt="Shoes"
+                        />
+                    </figure>
+                    <div class="card-body">
+                        <h2 class="card-title">Shoes!</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div class="card-actions justify-end">
+                            <button class="btn btn-primary">Buy Now</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="card bg-base-100 w-96 shadow-xl">
+                    <figure>
+                        <img
+                            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                            alt="Shoes"
+                        />
+                    </figure>
+                    <div class="card-body">
+                        <h2 class="card-title">Shoes!</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div class="card-actions justify-end">
+                            <button class="btn btn-primary">Buy Now</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="card bg-base-100 w-96 shadow-xl">
+                    <figure>
+                        <img
+                            src="https://img.daisyui.com/images/stock/photo-1606107557195-0e29a4b5b4aa.webp"
+                            alt="Shoes"
+                        />
+                    </figure>
+                    <div class="card-body">
+                        <h2 class="card-title">Shoes!</h2>
+                        <p>If a dog chews shoes whose shoes does he choose?</p>
+                        <div class="card-actions justify-end">
+                            <button class="btn btn-primary">Buy Now</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
