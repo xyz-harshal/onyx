cca98470cd2d7baffb83acf85ff107f2f74bee91
harshal-ishere <harshal011kamble@gmail.com>
2024-09-05 01:59:41 +0530

    did the client and something

--- client/src/routes/master/+page.svelte
+++ client/src/routes/master/+page.svelte
@@ -1,5 +1,6 @@
 <script>
     import "../../app.css";
+    import { data } from "./data.js";
     let img_arr = [
         "../src/pictures/img1.png",
         "../src/pictures/img2.png",
@@ -8,11 +9,12 @@
         "../src/pictures/img5.png",
         "../src/pictures/img6.png",
     ];
+    const getImageSrc = (meow) => `data:image/png;base64,${meow}`;
 </script>
 
-<div class="hero bg-base-200 pt-28 pb-18">
+<div class="hero bg-base-200 pt-40 pb-18">
     <div class="hero-content text-center">
-        <div class="max-w-md">
+        <div class="max-w-4xl">
             <h1 class="text-5xl font-bold">
                 Hyperspectral Anomaly Detection: A Powerful Tool for Data
                 Analysis
@@ -24,17 +26,49 @@
                 />
                 <button class="btn btn-primary btn-wide text-lg">Submit</button>
             </div>
-            <div class="divider"></div>
-            <div class="">
-                <p class="text-5xl font-bold pb-6">Output</p>
-                <p class="text-lg">The output images will appear here</p>
-            </div>
-            <div class="divider"></div>
         </div>
     </div>
 </div>
+<div class="flex flex-col bg-base-200 items-center px-32">
+    <p class="text-5xl font-bold pb-6">Output</p>
+    {#if data}
+        <div class="flex flex-row flex-wrap gap-6 justify-center">
+            {#each data.images as item}
+                <!-- The button to open modal -->
+                <a href="#my_modal_8"
+                    ><img
+                        src={getImageSrc(item)}
+                        alt=""
+                        width="400"
+                        height="400"
+                        class="rounded-xl"
+                    /></a
+                >
+
+                <!-- Put this part before </body> tag -->
+                <div class="modal" role="dialog" id="my_modal_8">
+                    <div class="modal-box">
+                        <img
+                            src={getImageSrc(item)}
+                            alt=""
+                            width="1000"
+                            height="1000"
+                            class="rounded-xl"
+                        />
+                        <div class="modal-action">
+                            <a href="#" class="btn">Close</a>
+                        </div>
+                    </div>
+                </div>
+            {/each}
+        </div>
+    {:else}
+        <p class="text-lg">The output images will appear here</p>
+    {/if}
+</div>
+
 <div class="bg-base-200 flex flex-col items-center pb-20">
-    <p class="font-bold text-5xl py-6">Examples</p>
+    <p class="font-bold text-5xl py-12">Examples</p>
     <div class="flex flex-row gap-10 mx-32 justify-center flex-wrap">
         {#each img_arr as img}
             <div class="card bg-base-100 w-96 shadow-xl">
